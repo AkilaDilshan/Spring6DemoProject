147e9e9a47a01c0e639a0bc27211aabe06b8ab5f
Akila Dilshan <akiladilshan92@gmail.com>
2023-08-01 16:17:57 +0530

    Initial Commit

---
+++ src/main/java/com/akila/Spring6DemoProject/Spring6DemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akila.Spring6DemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DemoProjectApplication.class, args);
+	}
+
+}
